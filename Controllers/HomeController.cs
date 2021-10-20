e0d98efdfc51116b025398f75c4104f003544c35
MrRedPanda <jorgeloredohdz@gmail.com>
2021-10-20 12:36:32 -0500

    Commit b4 tragedy

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="HomeController.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// </copyright>
+
+namespace Sample.IncidentBot.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// The home controller class.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Get the default content of home page.
+        /// </summary>
+        /// <returns>Default content.</returns>
+        ///
+        [HttpGet("/")]
+        public IActionResult Get()
+        {
+            return this.View("home");
+        }
+    }
+}
